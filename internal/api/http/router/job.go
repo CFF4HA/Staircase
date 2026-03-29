d82a670062ff2704808f3a15f7c2bf89818ee774
Diego Alba <62622191+DAlba-sudo@users.noreply.github.com>
2026-03-29 19:54:11 -0400

    Revert " feat: added rescan, groundwork for edit "

--- internal/api/http/router/job.go
+++ internal/api/http/router/job.go
@@ -12,51 +12,6 @@ import (
 	"github.com/CFF4HA/Staircase/internal/types"
 )
 
-func HandleJobRedo(w http.ResponseWriter, r *http.Request) error {
-	db := database.Database()
-
-	// has the ID already pre-filled in the request body
-	var request types.DatabaseJob
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return err
-	}
-
-	owner_id, err := getUserIdFromSession(r)
-	if err != nil {
-		core.Logger.Error("error fetching user id from session", "error", err)
-		return err
-	}
-
-	tx := db.Model(&types.DatabaseJob{}).Where("id = ?", request.ID).Where("owner_id = ?", owner_id).Preload("Metadata").First(&request)
-	if tx.Error != nil {
-		core.Logger.Error("error fetching job for redo", "error", tx.Error)
-		return err
-	}
-
-	request.Metadata = types.DatabaseJobMetadata{
-		ID:                 request.Metadata.ID,
-		JobId:              request.ID,
-		IsInitialized:      false,
-		IsInProgress:       false,
-		IsFinished:         false,
-		IsUploaded:         false,
-		LastScan:           request.Metadata.LastScan,
-		InjectedJavascript: "",
-		Result:             "",
-		Image:              "",
-		Error:              "",
-	}
-
-	tx = db.Save(&request.Metadata)
-	if tx.Error != nil {
-		core.Logger.Error("error resetting job metadata for redo", "error", tx.Error)
-		return err
-	}
-
-	core.Logger.Debug("job reset for redo", "job_id", request.ID, "owner_id", owner_id)
-	return nil
-}
-
 func HandleJobPUT(w http.ResponseWriter, r *http.Request) error {
 	db := database.Database()
 
@@ -95,72 +50,8 @@ func HandleJobPUT(w http.ResponseWriter, r *http.Request) error {
 	return nil
 }
 
-func HandleJobPATCH(w http.ResponseWriter, r *http.Request) error {
-	db := database.Database()
-
-	var request types.DatabaseJob
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		return err
-	}
-
-	// the owner based on the session
-	owner, err := getUserIdFromSession(r)
-	if err != nil {
-		return err
-	}
-
-	// the job must have an ID, and it must be owned by the owner
-	var job types.DatabaseJob
-	tx := db.Model(&types.DatabaseJob{}).
-		Where("id = ?", request.ID).
-		Where("owner_id = ?", owner).
-		Preload("Metadata").
-		Preload("Staircases").
-		First(&job)
-
-	if tx.Error != nil {
-		return err
-	}
-
-	// we now will do the "diffing" and determine what needs to be updated
-	if request.Name != job.Name {
-		job.Name = request.Name
-		if tx := db.Save(&job); tx.Error != nil {
-			return err
-		}
-	}
-
-	if request.Frequency != job.Frequency {
-		job.Frequency = request.Frequency
-		if tx := db.Save(&job); tx.Error != nil {
-			return err
-		}
-
-	}
-
-	if request.DatasourceId != job.DatasourceId {
-		job.DatasourceId = request.DatasourceId
-		if tx := db.Save(&job); tx.Error != nil {
-			return err
-		}
-	}
-
-	// now we will diff the staricase attributes
-	for i, staircase := range request.Staircases {
-		if job.Staircases[i].Declaration != staircase.Declaration {
-			job.Staircases[i].Declaration = request.Staircases[i].Declaration
-			if tx := db.Save(&job); tx.Error != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
 func HandleJobGET(w http.ResponseWriter, r *http.Request) error {
 	db := database.Database()
-
 	var request types.DatabaseJob
 	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		if err != io.EOF {
